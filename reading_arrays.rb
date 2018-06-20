6fc414d882cc2441ab7cd4876bd9014d07b27fbc
vaishiyer16 <vaishnavi.l.iyer@gmail.com>
2018-06-20 15:51:42 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -9,37 +9,37 @@ STUDENT_NAMES = [
 
 def first_student_by_index
   room = ["Adele", "Beyonce","Cardi B","Lady Gaga", "Nikki Minaj","Rihanna"]
-  puts room[0]
+  return room[0]
 end
 first_student_by_index
 
 def fourth_student_by_index
   room = ["Adele", "Beyonce","Cardi B","Lady Gaga", "Nikki Minaj","Rihanna"]
-  puts room[3]
+  return room[3]
 
 end
 fourth_student_by_index
 
 def last_student_by_index
   room = ["Adele", "Beyonce","Cardi B","Lady Gaga", "Nikki Minaj","Rihanna"]
-  puts room[5]
+  return room[5]
 end
 last_student_by_index
 
 def first_student_by_method
   room = ["Adele", "Beyonce","Cardi B","Lady Gaga", "Nikki Minaj","Rihanna"]
-  puts room.first
+  return room.first
 end
 first_student_by_method
 
 def last_student_by_method
 room = ["Adele", "Beyonce","Cardi B","Lady Gaga", "Nikki Minaj","Rihanna"]
-puts room.last
+return room.last
 end
 last_student_by_method
 
 def first_second_and_third_students
 room = ["Adele", "Beyonce","Cardi B","Lady Gaga", "Nikki Minaj","Rihanna"]
-puts room[0..2]
+return room[0..2]
 end
 first_second_and_third_students
