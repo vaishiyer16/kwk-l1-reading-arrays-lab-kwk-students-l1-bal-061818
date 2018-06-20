1d9ebe8f74ee7780f8f166bdad851528e79a4be0
vaishiyer16 <vaishnavi.l.iyer@gmail.com>
2018-06-20 15:54:31 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -8,38 +8,32 @@ STUDENT_NAMES = [
 ]
 
 def first_student_by_index
-  room = ["Adele", "Beyonce","Cardi B","Lady Gaga", "Nikki Minaj","Rihanna"]
-  return room[0]
+  return STUDENT_NAMES[0]
 end
 first_student_by_index
 
 def fourth_student_by_index
-  room = ["Adele", "Beyonce","Cardi B","Lady Gaga", "Nikki Minaj","Rihanna"]
-  return room[3]
+  return STUDENT_NAMES[3]
 
 end
 fourth_student_by_index
 
 def last_student_by_index
-  room = ["Adele", "Beyonce","Cardi B","Lady Gaga", "Nikki Minaj","Rihanna"]
-  return room[5]
+  return STUDENT_NAMES[5]
 end
 last_student_by_index
 
 def first_student_by_method
-  room = ["Adele", "Beyonce","Cardi B","Lady Gaga", "Nikki Minaj","Rihanna"]
-  return room.first
+  return STUDENT_NAMES.first
 end
 first_student_by_method
 
 def last_student_by_method
-room = ["Adele", "Beyonce","Cardi B","Lady Gaga", "Nikki Minaj","Rihanna"]
-return room.last
+return STUDENT_NAMES.last
 end
 last_student_by_method
 
 def first_second_and_third_students
-room = ["Adele", "Beyonce","Cardi B","Lady Gaga", "Nikki Minaj","Rihanna"]
-return room[0..2]
+return STUDENT_NAMES[0..2]
 end
 first_second_and_third_students
