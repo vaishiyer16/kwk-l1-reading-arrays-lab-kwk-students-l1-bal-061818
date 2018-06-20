ecb75c0d9851d8e50fb353d0ac64c8b985ff4803
vaishiyer16 <vaishnavi.l.iyer@gmail.com>
2018-06-20 15:50:03 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -8,25 +8,38 @@ STUDENT_NAMES = [
 ]
 
 def first_student_by_index
-  # Write a solution that returns the first student in the array student_names
+  room = ["Adele", "Beyonce","Cardi B","Lady Gaga", "Nikki Minaj","Rihanna"]
+  puts room[0]
 end
+first_student_by_index
 
 def fourth_student_by_index
-  # Write a solution that returns the first student in the array student_names
+  room = ["Adele", "Beyonce","Cardi B","Lady Gaga", "Nikki Minaj","Rihanna"]
+  puts room[3]
+
 end
+fourth_student_by_index
 
 def last_student_by_index
-  # Write a solution that returns the first student in the array student_names
+  room = ["Adele", "Beyonce","Cardi B","Lady Gaga", "Nikki Minaj","Rihanna"]
+  puts room[5]
 end
+last_student_by_index
 
 def first_student_by_method
-  # Write a solution that returns the first student using the built in .first method
+  room = ["Adele", "Beyonce","Cardi B","Lady Gaga", "Nikki Minaj","Rihanna"]
+  puts room.first
 end
+first_student_by_method
 
 def last_student_by_method
-  # Write a solution that returns the first student using the built in .last method
+room = ["Adele", "Beyonce","Cardi B","Lady Gaga", "Nikki Minaj","Rihanna"]
+puts room.last
 end
+last_student_by_method
 
 def first_second_and_third_students
-  # Write a solution that returns the first, second and third students
+room = ["Adele", "Beyonce","Cardi B","Lady Gaga", "Nikki Minaj","Rihanna"]
+puts room[0..2]
 end
+first_second_and_third_students
